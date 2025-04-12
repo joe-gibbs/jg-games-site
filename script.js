d6e10742a10e7fe96430395e53d2152f2315b665
Joe Gibbs <joe.gibbs0@gmail.com>
2025-04-12 16:02:47 +1000

    fix: update script

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 import { tsParticles } from "@tsparticles/engine";
-// import { loadAll } from "tsparticles-all"; // if you are going to use `loadAll`, install the "tsparticles-all" package too.
-// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
 import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
-// import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
 
 async function loadParticles(options) {
     await loadBasic(tsParticles); // Load the basic preset bundle
